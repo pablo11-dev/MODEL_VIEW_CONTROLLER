3d342db9ac187af6a75ed22a6a89e1e331021ee5
pablo11-dev <pablofr2018@gmail.com>
2023-10-10 15:55:53 -0300

    .15:55

--- View.py
+++ View.py
@@ -8,27 +8,28 @@ while sair == 0:
     print("    SOFTWARE DE TO-DO")
     print(" ________________________")
     print("|[1] -> ADICIONAR TAREFA |")
-    print("|[Q] -> LISTAR TAREFAS   |")
+    print("|[2] -> LISTAR TAREFAS   |")
     print("|[3] -> EXCLUIR TAREFAS  |")
     print("|[4] -> SAIR             |")
     print("|________________________|")
     print("")
 
     #Convertendo o input para inteiro
+
     menu = input("-> ") 
     
     
     match menu:
-        case 1:
+        case "1":
             os.system("cls")
             tarefa = input("Adicione uma TAREFA -> ")
             adicionarTarefa = ControllerAdicionarTarefa(tarefa)
             os.system("pause")
-        case 2:
+        case "2":
             os.system("cls")
             listarTarefas = ControllerListarTarefas()
             os.system("pause")
-        case 3:
+        case "3":
             os.system("cls")
             listarTarefas = ControllerListarTarefas()
             excluir = input("Qual o indice da tarefa que deseja excluir?\n-> ")
@@ -36,7 +37,7 @@ while sair == 0:
             print("\nLista:")
             listarTarefas = ControllerListarTarefas()
             os.system("pause")
-        case 4:
+        case "4":
             sair = 1
             print("Saindo...")
             os.system("pause")
